1b8bf71a5127343219dd0105a0a574b4ab940b1b
Ryan K. Clark <mail.w3geek@gmail.com>
2020-06-28 16:22:41 -0400

    Task #4 - Added TestCases to ensure homepage doesn't break because of my (Ryan's) stupidity.

--- tests/app/Controllers/HomeTest.php
+++ tests/app/Controllers/HomeTest.php
@@ -1,15 +1,23 @@
 <?php namespace App\Controllers;
 
-	use CodeIgniter\Test\CIUnitTestCase;
+	use CodeIgniter\Test\ControllerTester;
 	use PHPUnit\Framework\TestCase;
 
 	class HomeTest extends TestCase
 	{
-		public function testTrueIsNotFalse() {
-			$this->assertNotEquals(true, false);
+		use ControllerTester;
+
+		public function testHomepage_returnsOkStatus(): void {
+			$response = $this->withUri('http://whiskeybox.local/')
+							 ->controller(Home::class)
+							 ->execute('index');
+			$this->assertTrue($response->isOK());
 		}
 
-		public function testTrueIsTrue() {
-			$this->assertEquals(true, true);
+		public function testHomepageTitle_returnsGivenText(): void {
+			$response = $this->withUri('http://whiskeybox.local/')
+							 ->controller(Home::class)
+							 ->execute('index');
+			$this->assertTrue($response->see('Welcome to CodeIgniter 4.0.3', 'h1'));
 		}
 	}
